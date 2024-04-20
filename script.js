8379c1a26baa83ade8517c34a9960bca5f3f5fb1
Nitish Jaiswal <141999440+nitishjaiswal2002@users.noreply.github.com>
2024-04-20 20:52:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //your JS code here. If required.
-.pass{
-	border:1px solid #ccc;
-	padding:0.5rem;
-	font-size:1rem;
+.password-input {
+    border: 1px solid #ccc;
+    padding: 0.5rem;
+    font-size: 1rem;
 }
